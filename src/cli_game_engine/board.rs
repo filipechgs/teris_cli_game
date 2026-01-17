c1ccb2020d70b4d9084f33d4a34d7fe7c9303e2a
Filipe Lima Guedes chagas <filipechgs@gmail.com>
2026-01-17 14:53:18 -0300

    add: adiciona funcionalidade de exibição da próxima peça

--- src/cli_game_engine/board.rs
+++ src/cli_game_engine/board.rs
@@ -7,7 +7,9 @@ pub mod board {
         pieces: [Shape; 7],
         pub board_20x28: [[u8; 20]; 28],
         pub active_piece: Shape,
+        pub next_piece: Shape,
         pub piece_start_position_in_board_y_x: [usize; 2],
+        is_first_piece: bool,
     }
 
     impl GameBoard {
@@ -30,36 +32,58 @@ pub mod board {
                 ],
                 board_20x28: [line; BOARD_HEIGHT],
                 active_piece: Shape::new_i(),
+                next_piece: Shape::new_i(),
                 piece_start_position_in_board_y_x: [0, 8],
+                is_first_piece: true,
             }
         }
 
         pub fn display(&self) {
             clearscreen::clear().expect("Failed to clear screen");
-            
+
+            let next_piece = self.next_piece.get_current_shape();
+
             println!(" ____________________ ");
-            
+
+            for (_row_i, row) in next_piece.iter().enumerate() {
+                let line_slice: &str =
+                    std::str::from_utf8(row).expect("Erro: O array de bytes não é UTF-8 válido.");
+                println!("|        {}        |", line_slice);
+            }
+            println!(" ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ ");
+
+            println!(" ____________________ ");
+
             for line in self.board_20x28 {
                 // Transforma cada array em uma string, sem separação entre os caracteres
                 let line_slice: &str =
-                std::str::from_utf8(&line).expect("Erro: O array de bytes não é UTF-8 válido.");
+                    std::str::from_utf8(&line).expect("Erro: O array de bytes não é UTF-8 válido.");
                 println!("|{}|", line_slice);
             }
             println!(" ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ ");
         }
 
+        fn get_random_piece(&self) -> Shape {
+            self.pieces[rand::random_range(0..self.pieces.len())]
+        }
+
         pub fn select_new_piece(&mut self) {
-            self.active_piece = self.pieces[rand::random_range(0..self.pieces.len())];
+            if self.is_first_piece {
+                self.active_piece = self.get_random_piece();
+                self.next_piece = self.get_random_piece();
+                self.is_first_piece = false;
+            }
+
+            self.active_piece = self.next_piece;
+            self.next_piece = self.get_random_piece()
         }
 
         pub fn add_piece(&mut self) {
             self.piece_start_position_in_board_y_x = [0, 8];
-
             let piece_shape: Matrix4x4 = self.active_piece.get_current_shape();
 
             for (piece_row_i, piece_line) in piece_shape.iter().enumerate() {
                 for (piece_col_i, &piece_cell) in piece_line.iter().enumerate() {
-
                     let board_row = self.piece_start_position_in_board_y_x[0] + piece_row_i;
                     let board_col = self.piece_start_position_in_board_y_x[1] + piece_col_i;
 
@@ -72,7 +96,8 @@ pub mod board {
 
         pub fn shape_fall(&mut self) -> bool {
             let piece_shape: Matrix4x4 = self.active_piece.get_current_shape();
-            let piece_last_char_position_y_x: [usize; 2] = self.active_piece.get_last_char_position_y_x();
+            let piece_last_char_position_y_x: [usize; 2] =
+                self.active_piece.get_last_char_position_y_x();
 
             // Itera sobre a peça posicionada no tabuleiro e identifica se ela pode cair
             for (piece_row_index, piece_line) in piece_shape.iter().enumerate() {
@@ -81,16 +106,19 @@ pub mod board {
                         continue;
                     }
 
-                    let next_board_row = self.piece_start_position_in_board_y_x[0] + piece_row_index + 1;
-                    
+                    let next_board_row =
+                        self.piece_start_position_in_board_y_x[0] + piece_row_index + 1;
+
                     if next_board_row >= 28 {
                         return false;
                     }
 
                     let board_col = self.piece_start_position_in_board_y_x[1] + piece_col_index;
-                    
+
                     // Paraliza se: essa for a última linha da figura e a próxima linha do tabuleiro tiver um caractere 35 embaixo de algum caractere da figura
-                    if  piece_row_index >= piece_last_char_position_y_x[0] && self.board_20x28[next_board_row][board_col] == 35 {
+                    if piece_row_index >= piece_last_char_position_y_x[0]
+                        && self.board_20x28[next_board_row][board_col] == 35
+                    {
                         return false;
                     }
                 }
@@ -100,7 +128,9 @@ pub mod board {
             for (piece_row_index, piece_line) in piece_shape.iter().enumerate() {
                 for (piece_col_index, &piece_cell) in piece_line.iter().enumerate() {
                     // Evitar apagamento de outras peças lateráis
-                    if piece_row_index > 3 && piece_cell == 32 {continue;}
+                    if piece_row_index > 3 && piece_cell == 32 {
+                        continue;
+                    }
 
                     if piece_row_index > piece_last_char_position_y_x[0] {
                         break;
@@ -120,15 +150,14 @@ pub mod board {
 
             for (piece_row_index, piece_line) in piece_shape.iter().enumerate() {
                 for (piece_col_index, &piece_cell) in piece_line.iter().enumerate() {
-
                     let board_row = self.piece_start_position_in_board_y_x[0] + piece_row_index;
                     let board_col = self.piece_start_position_in_board_y_x[1] + piece_col_index;
-                    
+
                     if board_row < 28 {
                         if self.board_20x28[board_row][board_col] == 32 && piece_cell == 35 {
                             // Esceve a célula da figura em queda
                             self.board_20x28[board_row][board_col] = 35
-                        }                    
+                        }
                     }
                 }
             }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,7 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    // Cria tabuleiro do jogo
     let mut game_board = GameBoard::new_20x28();
-    game_board.select_new_piece();
-    game_board.add_piece();
-    game_board.display();
   
     loop {
         if !game_board.shape_fall() { 
@@ -17,6 +13,6 @@ fn main() {
         }
         
         game_board.display();
-        thread::sleep(Duration::from_millis(200));
+        thread::sleep(Duration::from_millis(250));
     }
 }
